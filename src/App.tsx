60af1780a82b33bbf134b112ba0aabaa1d5f2f0b
Zimuzo Obi <obizimuzo@gmail.com>
2023-08-24 10:30:03 +0100

    layout postcode

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
         element: <Home /> 
       },
       {
-        path: '/postcode/:code',
+        path: '/:postcode',
         element: <Postcode />
       }
     ]
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
+
 function Home() {
-    return <section className="flex items-center h-screen">
+
+    const [query, setQuery] = useState<string>('')
+    
+    return <section className="flex items-center h-screen uppercase">
         <form className="flex flex-col space-y-2 w-[760px] mx-auto px-4 ">
             <div className="flex flex-col space-y-2">
                 <label htmlFor="search" className="text-gray-500 text-lg">Search for a postcode</label>
-                <input type="text" placeholder="Postcode Search" className="border border-gray-300 w-full p-4 rounded-md focus:outline-none focus:ring focus:border-blue-300"/>
+                <input type="text" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Postcode Search" className="border border-gray-300 w-full p-4 rounded-md focus:outline-none focus:ring focus:border-blue-300"/>
             </div>
-            <button type="submit" className="bg-blue-600 text-white p-4 w-max rounded-md">Submit</button>
+            <button type="submit" className="bg-blue-600 text-white p-4 sm:w-max rounded-md">Submit</button>
         </form>
     </section>
 }
